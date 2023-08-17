ff4e4ff0166c75d6f17627152314ca172456847d
Vanille-fraise <49752245+Vanille-fraise@users.noreply.github.com>
2023-08-17 13:13:30 +0200

    best estim

--- src/builder/algorithm_mod/item_estimator.rs
+++ src/builder/algorithm_mod/item_estimator.rs
@@ -15,7 +15,7 @@ pub struct ItemEstimator;
 impl ItemEstimator {
     pub fn roxx_based_estimation<'a>(container: &'a DataContainer, attack: &Attack) -> Vec<&'a Item> {
         let mut res = vec![];
-        let mut build = Build::new_with_stats(Stats::from_map_stats(HashMap::from([(BaseStat::Puissance, 1400i64), (BaseStat::DoMulti, 160)]).iter()));
+        let mut build = Build::new_with_stats(Stats::from_map_stats(HashMap::from([(BaseStat::Puissance, 180i64), (BaseStat::DoMulti, 160)]).iter()));
         let mut estimations = vec![];
         let used_slot = ItemSlot::SlotDofusPrysmaradite;
         for itm in &container.items {
@@ -24,15 +24,24 @@ impl ItemEstimator {
             build.remove_item(&used_slot);
         }
         estimations.sort_by(|e1, e2| e2.0.cmp(&e1.0));
-        let mut by_cat: HashMap<ItemType, Vec<&Item>> = HashMap::new();
-        ItemType::iter().for_each(|is| { by_cat.insert(is, vec![]); });
-        estimations.iter().for_each(|x| { by_cat.get_mut(&x.1.item_type).unwrap().push(x.1); });
-        while res.len() < container.items.len() {
-            ItemType::iter().for_each(|i| {
-                let v = by_cat.get_mut(&i).unwrap();
-                (!v.is_empty()).then(|| res.push(v.remove(0)));
-            });
+        let mut by_cat: HashMap<ItemSlot, Vec<&Item>> = HashMap::new();
+        ItemSlot::iter().for_each(|is| { by_cat.insert(is, vec![]); });
+        let mut by_cat_i: HashMap<ItemType, usize> = HashMap::new();
+        ItemType::iter().for_each(|is| { by_cat_i.insert(is, 0); });
+        for (_, itm) in estimations {
+            let slots = ItemSlot::corresponding_to_item_type(&itm.item_type);
+            by_cat.get_mut(&slots[by_cat_i.get(&itm.item_type).unwrap() % slots.len()]).unwrap().push(&itm);
+            *by_cat_i.get_mut(&itm.item_type).unwrap() += 1;
         }
+        while res.len() < container.items.len(){
+            for (_, cur_items) in by_cat.iter_mut() {
+                if !cur_items.is_empty(){
+                    res.push(cur_items.remove(0));
+                }
+            }
+        }
+        let names : &Vec<&String>=  &res.split_at(32).0.iter().map(|i| &i.name).collect();
+        println!("First 32 items: {:?}", &names);
         return res;
     }
 }
