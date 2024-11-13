104b0b763d3249152307e484cee88ba0b59e0c22
Vanille-fraise <edouard.farrando@epita.fr>
2024-11-14 00:13:05 +0100

    fix to breeds data --release

--- src/web/api/roxx_builder/services/breed_list_service.rs
+++ src/web/api/roxx_builder/services/breed_list_service.rs
@@ -5,6 +5,6 @@ use crate::web::api::roxx_builder::services::response_wrapper::ResponseWrapper;
 #[get("/api/breeds")]
 pub async fn get_breeds(app_data: web::Data<AppState>) -> impl Responder {
     println!("Providing breeds.");
-    let result = &app_data.data_container.spell_variants;
+    let result = &app_data.data_container.breeds;
     HttpResponse::Ok().json(ResponseWrapper {status: "succes".to_string(), data: &result})
 }
